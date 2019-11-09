da8c53c165fb04a362a0a7baede7369a33e01c95
viviancan <vcnales@gmail.com>
2019-11-09 12:36:29 -0600

    worked on page redirect + portfolio page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-const $ = require('jquery');
-window.jQuery = $
+// const $ = require('jquery');
+//
+// window.jQuery = $
 
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -56,14 +57,14 @@ getBusinesses().then((businesses) => {
 });
 
 
-getOwners().then((owners) => {
-	owners.forEach(({id, businessId, firstName, lastName}) => {
-		console.log(`id#${id} - ${businessId} - ${firstName} - ${lastName}`);
-	});
-}).catch((error) => {
-	alert('Oh no! Something went wrong.\nCheck the console for details.')
-	console.log(error);
-});
+// getOwners().then((owners) => {
+// 	owners.forEach(({id, businessId, firstName, lastName}) => {
+// 		console.log(`id#${id} - ${businessId} - ${firstName} - ${lastName}`);
+// 	});
+// }).catch((error) => {
+// 	alert('Oh no! Something went wrong.\nCheck the console for details.')
+// 	console.log(error);
+// });
 
 // $("#main-search").on('click', function (event) {
 //   event.preventDefault();
@@ -124,7 +125,7 @@ function createBusinessCardHtml(business){
 	html += '<p class="card-text">'+ business.description +'</p>';
 	html += '</div>';
 	html += '<div class="card-body text-center">';
-	html += '<a href="#" class="card-link"> Learn More</a>';
+	html += '<a href="" class="card-link" data-id="'+business.id +'"> Learn More</a>';
 	html += '</div>';
 	html += '</div>';
 	html += '</div>';
@@ -165,3 +166,111 @@ function createBusinessTable(){
 // $( "#tags" ).autocomplete({
 //   source: availableTags
 // });
+
+
+
+$('.card-link').on("click", function(event){
+	event.preventDefault()
+	console.log("card link button clicked");
+	var businessId = $(this).attr("data-id")
+	console.log(businessId);
+	var selectedBusinessObject = businessList[businessId - 1];
+
+	var queryString = $.param({id: businessId})
+
+	window.location.replace("/profile.html?" + queryString);
+
+
+
+
+
+	// $().redirect('/profile.html', selectedBusinessObject)
+	// $("#test-container").html("test test test")
+
+
+	// $.ajax({
+	// 	type: "GET",
+	// 	url: "/api/business",
+	// 	data: businessId,
+	// 	dataType: "json",
+	// 	success: function(data, textStatus) {
+	// 			// data.redirect contains the string URL to redirect to
+	//
+	// 		console.log(data);
+	// 		window.location.href = "/profile.html";
+	//
+	// 	}
+	// });
+
+
+})
+
+
+$.urlParam = function (name) {
+	var results = new RegExp('[\?&]' + name + '=([^&#]*)')
+		.exec(window.location.search);
+
+	return (results !== null) ? results[1] || 0 : false;
+}
+
+var profileBusinessId = $.urlParam('id')
+
+var profileBusiness = businessList[profileBusinessId-1];
+console.log(profileBusiness);
+var businessAddress = profileBusiness.address
+
+// var geocoder = new google.maps.Geocoder();
+// geocoder.geocode( { 'address': businessAddress}, function(results, status) {
+// 	if (status == 'OK') {
+// 		console.log(results);
+// 		// map.setCenter(results[0].geometry.location);
+// 		// var marker = new google.maps.Marker({
+// 		// 	map: map,
+// 		// 	position: results[0].geometry.location
+// 		// });
+// 	} else {
+// 		alert('Geocode was not successful for the following reason: ' + status);
+// 	}
+// });
+
+
+// googleMapsClient.geocode({
+// 	address: '1600 Amphitheatre Parkway, Mountain View, CA'
+// }, function(err, response) {
+// 	if (!err) {
+// 		console.log(response.json.results);
+// 	}
+// });
+
+$('#profile-title').html(profileBusiness.name)
+
+var businessHtml = "";
+businessHtml += '<ol class="breadcrumb">'
+businessHtml += '<li class="breadcrumb-item">'
+businessHtml += '<a href="directory.html">Home</a>'
+businessHtml += '</li>'
+businessHtml += '<li class="breadcrumb-item active">Portfolio Item</li>'
+businessHtml += '</ol>'
+businessHtml += '<h1 class="mt-4 mb-3" id="">' + profileBusiness.name + ' ';
+// businessHtml += '<small>'+ profileBusiness.firstName + ' ' + profileBusiness.lastName  +'</small>'
+businessHtml += '</h1>'
+businessHtml += '<div class="row">'
+businessHtml += '<div class="col-md-6">'
+businessHtml += '<img class="img-fluid" src="img/'+ profileBusiness.imageUrl +'" alt="">'
+businessHtml += '<div id="map" style="width: 320px; height: 480px;"></div>';
+businessHtml += '</div>'
+businessHtml += '<div class="col-md-6">'
+businessHtml += '<h3 class="my-3"><img id="affiliation-logo" src="img/'+ profileBusiness.affiliationLogoUrl +'" alt=""> '+profileBusiness.firstName + ' ' + profileBusiness.lastName + ' - <span>'+ profileBusiness.affiliation +'</span></h3>'
+// businessHtml += '<p>'+ profileBusiness.affiliation +'</p>'
+
+businessHtml += '<p>'+ profileBusiness.description +'</p>'
+businessHtml += '<hr>'
+businessHtml += '<ul id="project-dets-list">'
+businessHtml += '<li><strong>Address: </strong>'+ profileBusiness.address + '</li>'
+businessHtml += '<li><strong>Phone: </strong>'+ profileBusiness.phone + '</li>'
+businessHtml += '<li><strong>Website: </strong><a target="_blank" href="'+ profileBusiness.website +'">'+ profileBusiness.website + '</a></li>'
+businessHtml += '</ul>'
+businessHtml += '</div>'
+businessHtml += '</div>'
+
+$("#profile-container").html(businessHtml)
